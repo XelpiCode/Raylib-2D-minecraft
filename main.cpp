881f5f1deda6f98151721308b9fce7361c2c862e
Xelpi <ankith3690@gmail.com>
2025-07-23 21:30:40 +0530

    Made blocks data structs

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #include <iostream>
 #include <bits/ostream.tcc>
 
+struct BlockData {
+    float x, y;
+    Texture2D texture;
+};
+
 void DrawTextureOnSquareSimple(const Texture2D &texture, float posX, float posY, float size, Color tint)
 {
     Rectangle sourceRec = { 0.0f, 0.0f, static_cast<float>(texture.width), static_cast<float>(texture.height) };
@@ -13,21 +18,20 @@ void DrawTextureOnSquareSimple(const Texture2D &texture, float posX, float posY,
     DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, tint);
 }
 
-bool AlreadyInVector(const std::vector<std::array<float, 2>> &placedBlock, std::array<float, 2> position) {
+bool AlreadyInVector(const std::vector<BlockData> &placedBlock, BlockData position) {
     for (auto & block : placedBlock) {
-        if (block[0] == position[0] && block[1] == position[1]) {
+        if (block.x == position.x && block.y == position.y) {
             return true;
         }
     }
     return false;
 }
 
-void PlaceBlocks(const std::vector<std::array<float, 2>> &placedBlocks, const Texture2D &texture) {
+void PlaceBlocks(const std::vector<BlockData> &placedBlocks) {
     for (auto & placedBlock : placedBlocks) {
-        DrawTextureOnSquareSimple(texture, placedBlock[0], placedBlock[1], 100, WHITE);
+        DrawTextureOnSquareSimple(placedBlock.texture, placedBlock.x, placedBlock.y, 100, WHITE);
     }
 }
-
 
 int main() {
     constexpr int screenWidth = 1920;
@@ -39,7 +43,7 @@ int main() {
 
     Texture2D grassBlock = LoadTexture("C:/Users/ankit/CLionProjects/raylib game/Textures/grass.png");
 
-    std::vector<std::array<float, 2>> placedBlocks;
+    std::vector<BlockData> placedBlocks;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -48,15 +52,15 @@ int main() {
 
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             Vector2 mousePos = GetMousePosition();
-            std::array<float, 2> newBlockPosition = {roundf((mousePos.x - 50)/100)*100 , roundf((mousePos.y - 50)/100)*100 };
+            BlockData newBlockPosition = {roundf((mousePos.x - 50)/100)*100 , roundf((mousePos.y - 50)/100)*100, grassBlock};
 
             if (!AlreadyInVector(placedBlocks, newBlockPosition)) {
                 placedBlocks.push_back(newBlockPosition);
-                std::cout << "placed block at " << newBlockPosition[0] << " " << newBlockPosition[1] << std::endl;
+                std::cout << "placed block at " << newBlockPosition.x << " " << newBlockPosition.y << std::endl;
             }
         }
 
-        PlaceBlocks(placedBlocks, grassBlock);
+        PlaceBlocks(placedBlocks);
 
         EndDrawing();
     }
